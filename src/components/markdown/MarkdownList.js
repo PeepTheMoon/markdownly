c3ce6202a234fc04510e6a0aefd66c0ef6278fd8
Lucia Brammer <brammerl96@gmail.com>
2020-08-06 14:59:58 -0700

    adding delete button

--- src/components/markdown/MarkdownList.js
+++ src/components/markdown/MarkdownList.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { getMarkdownTitles } from '../../selectors/selectors';
-import { useSelector } from '../../hooks/Context';
+import { useSelector, useDispatch } from '../../hooks/Context';
+import { deleteMarkdown } from '../../actions/reducerActions';
+
 
 const MarkdownList = () =>  {
-
+  const dispatch = useDispatch()
   const markdownList = useSelector(getMarkdownTitles);
 
   const markdownListElements = markdownList.map((title, i) => (
     <li key={i}>
       <p>{title}</p>
+      <button value={title} onClick={({ target }) => dispatch(deleteMarkdown(target.value))}>Delete</button>
     </li>
   ));
-    
+  
   return (
     <ul>
       {markdownListElements}    
